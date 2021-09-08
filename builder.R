7b79824b403624c995986da37d236df3aa87ce2e
ainsausti008 <ainsausti008@gmail.com>
2021-09-08 15:33:43 +0200

    rearrange builder base in monthly arrangement

--- builder.R
+++ builder.R
@@ -2,7 +2,7 @@
 # Data frame building for applying time-dependent survival models
 # Initial data is in PROJECT_RDATA: baseJoinModel
 # farmacos_traye data is merged to PROJECT_RDATA
-# Data is organized in monthly chunks
+# Data organized in monthly chunks
 # Time varying variables are added
 
 
@@ -19,7 +19,6 @@ FOLLOW_UP = 365
 
 # load sources
 source(paste0(BUILDING_SCRIPTS_PATH, 'baseJoinModel_farmaco_fusion.R'), encoding = 'UTF-8')
-source(paste0(BUILDING_SCRIPTS_PATH, 'prescription_aggregation.R'), encoding = 'UTF-8')
 source(paste0(BUILDING_SCRIPTS_PATH, 'montly_arrangement.R'), encoding = 'UTF-8')
 source(paste0(BUILDING_SCRIPTS_PATH, 'time_varying_covariates.R'), encoding = 'UTF-8')
 
@@ -33,18 +32,14 @@ df_farmacos <- constructedBases::farmacos_traye
 baseJoinModel_0 <- preprocess_baseJoinModel(baseJoinModel)
 df_farmacos <- preprocess_farmacos(df_farmacos, PROJECT_DRUGS)
 baseJoinModel_1 <- merge_farmacos(baseJoinModel_0, df_farmacos)
-baseJoinModel1 <- process_baseJoinModel1(baseJoinModel_1, duration=FOLLOW_UP)
-
-
-# df with drug prescriptions aggregated
-baseJoinModel2 <- aggregate_prescriptions(baseJoinModel1)
+baseJoinModel_2 <- process_baseJoinModel1(baseJoinModel_1, duration=FOLLOW_UP)
 
 
 # Data organized in monthly chunks
-baseJoinModel3 <- rearranged_in_months(baseJoinModel1)
+baseJoinModel1_0 <- rearranged_in_months(baseJoinModel_2)
+
 
 # df with time varying variables added
 baseJoinModel3_0 <- timevarying1(baseJoinModel2)
 baseJoinModel3_1 <- timevarying2(baseJoinModel2_0)
 baseJoinModel3_2 <- timevarying2(baseJoinModel2_0)
-# write.csv(baseJoinModel3_2, file='final_data.csv', row.names=FALSE)
--- building/montly_arrangement.R
+++ building/montly_arrangement.R
@@ -11,6 +11,52 @@ print ('montly_arrangement OK')
 #'
 #' @examples
 rearranged_in_months <- function(df){
-  
+  baseJoinModel1 <- scale_startend(baseJoinModel_2)
+  baseJoinModel2 <- divide_monthly_periods(baseJoinModel1)
+  baseJoinModel2 <- build_monthly_data(baseJoinModel1)
   return (df)
+}
+
+
+#' Add time var
+#'
+#' @param df 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+scale_startend <- function(df){
+  df <- df %>%
+    mutate(start_time = (start - falta_ing1 + 1)/30) %>%
+    mutate(end_time = (end - falta_ing1 + 1)/30)
+  df$start_time <- as.numeric(df$start_time, units="days")
+  df$end_time <- as.numeric(df$end_time, units="days")
+  return (df)
+}
+
+
+#' divide records in monthly periods
+#'
+#' @param df 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+divide_monthly_periods <- function(df){
+  return (df)
+}
+
+
+#' build monthly distributed data
+#'
+#' @param df 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+build_monthly_data <- function(df){
+  return(df)
 }
