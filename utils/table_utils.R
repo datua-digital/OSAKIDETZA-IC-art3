3063a1c3feff79eadcf8b4c18af34649becc2768
ainsausti008 <ainsausti008@gmail.com>
2021-10-20 12:24:23 +0200

    linting summary table

--- utils/table_utils.R
+++ utils/table_utils.R
@@ -1,106 +1,100 @@
 
 
 
-summary_table <- function(M1, M2, M3){
-  
+summary_table <- function(m1, m2, m3) {
   # Function for the generation of Table 3 of the manuscript.
-  
   #-------------#
   # Left column #
   #-------------#
-  value <- rbind(t(t(M1$postMeans$betas)),
-                 t(t(M1$postMeans$sigma)),
-                 t(t(M1$postMeans$gammas)),
-                 t(t(M1$postMeans$alphas)),
+  value <- rbind(t(t(m1$postMeans$betas)),
+                 t(t(m1$postMeans$sigma)),
+                 t(t(m1$postMeans$gammas)),
+                 t(t(m1$postMeans$alphas)),
                  NA)
-  lower <- rbind(t(t(M1$CIs$betas[1,])),
-                 t(t(M1$CIs$sigma[1,])),
-                 t(t(M1$CIs$gammas[1,])),
-                 t(t(M1$CIs$alphas[1,])),
+  lower <- rbind(t(t(m1$CIs$betas[1, ])),
+                 t(t(m1$CIs$sigma[1, ])),
+                 t(t(m1$CIs$gammas[1, ])),
+                 t(t(m1$CIs$alphas[1, ])),
                  NA)
-  upper <- rbind(t(t(M1$CIs$betas[2,])),
-                 t(t(M1$CIs$sigma[2,])),
-                 t(t(M1$CIs$gammas[2,])),
-                 t(t(M1$CIs$alphas[2,])),
+  upper <- rbind(t(t(m1$CIs$betas[2, ])),
+                 t(t(m1$CIs$sigma[2, ])),
+                 t(t(m1$CIs$gammas[2, ])),
+                 t(t(m1$CIs$alphas[2, ])),
                  NA)
-  intervals <- cbind(value,lower,upper)
-  colnames(intervals) <- c('value', 'lower', 'upper')
+  intervals <- cbind(value, lower, upper)
+  colnames(intervals) <- c("value", "lower", "upper")
   CI <- NULL
-  for(i in 1:10){
-    CI <- c(CI,paste0('(',round(lower[i],4),';',round(upper[i],4),')'))
+  for (i in 1:10) {
+    CI <- c(CI, paste0("(", round(lower[i], 4), ";", round(upper[i], 4), ")"))
   }
-  TM1 <- data.frame('Mean' = round(value,4), CI)
-  colnames(TM1) <- c('Mean', '95% CI')
-  rownames(TM1) <- c('Intercept', '$B_n(t,lambda_1)$', '$B_n(t,lambda_2)$',
-                     '$B_n(t,lambda_3)$', '$B_n(t,lambda_4)$', 'sigma_eps',
-                     'Edad', 'Sexo', 'Current Value', 'Slope')
-  
+  Tm1 <- data.frame("Mean" = round(value, 4), CI)
+  colnames(Tm1) <- c("Mean", "95% CI")
+  rownames(Tm1) <- c("Intercept", "$B_n(t,lambda_1)$", "$B_n(t,lambda_2)$",
+                     "$B_n(t,lambda_3)$", "$B_n(t,lambda_4)$", "sigma_eps",
+                     "Edad", "Sexo", "Current Value", "Slope")
   #----------------#
   # Central column #
   #----------------#
-  value <- rbind(t(t(M2$postMeans$betas)),
-                 t(t(M2$postMeans$sigma)),
-                 t(t(M2$postMeans$gammas)),
-                 t(t(M2$postMeans$alphas)),
-                 t(t(M2$postMeans$Dalphas)))
-  lower <- rbind(t(t(M2$CIs$betas[1,])),
-                 t(t(M2$CIs$sigma[1,])),
-                 t(t(M2$CIs$gammas[1,])),
-                 t(t(M2$CIs$alphas[1,])),
-                 t(t(M2$CIs$Dalphas[1,])))
-  upper <- rbind(t(t(M2$CIs$betas[2,])),
-                 t(t(M2$CIs$sigma[2,])),
-                 t(t(M2$CIs$gammas[2,])),
-                 t(t(M2$CIs$alphas[2,])),
-                 t(t(M2$CIs$Dalphas[2,])))
-  intervals <- cbind(value,lower,upper)
-  colnames(intervals) <- c('value', 'lower', 'upper')
+  value <- rbind(t(t(m2$postMeans$betas)),
+                 t(t(m2$postMeans$sigma)),
+                 t(t(m2$postMeans$gammas)),
+                 t(t(m2$postMeans$alphas)),
+                 t(t(m2$postMeans$Dalphas)))
+  lower <- rbind(t(t(m2$CIs$betas[1, ])),
+                 t(t(m2$CIs$sigma[1, ])),
+                 t(t(m2$CIs$gammas[1, ])),
+                 t(t(m2$CIs$alphas[1, ])),
+                 t(t(m2$CIs$Dalphas[1, ])))
+  upper <- rbind(t(t(m2$CIs$betas[2, ])),
+                 t(t(m2$CIs$sigma[2, ])),
+                 t(t(m2$CIs$gammas[2, ])),
+                 t(t(m2$CIs$alphas[2, ])),
+                 t(t(m2$CIs$Dalphas[2, ])))
+  intervals <- cbind(value, lower, upper)
+  colnames(intervals) <- c("value", "lower", "upper")
   CI <- NULL
-  for(i in 1:10){
-    CI <- c(CI,paste0('(',round(lower[i],4),';',round(upper[i],4),')'))
+  for (i in 1:10) {
+    CI <- c(CI, paste0("(", round(lower[i], 4), ";", round(upper[i], 4), ")"))
   }
-  TM2 <- data.frame('Mean' = round(value,4), CI)
-  colnames(TM2) <- c('Mean', '95% CI')
-  rownames(TM2) <- c('Intercept', '$B_n(t,lambda_1)$', '$B_n(t,lambda_2)$',
-                     '$B_n(t,lambda_3)$', '$B_n(t,lambda_4)$', 'sigma_eps',
-                     'Edad', 'Sexo', 'Current Value', 'Slope')
-  
+  Tm2 <- data.frame("Mean" = round(value, 4), CI)
+  colnames(Tm2) <- c("Mean", "95% CI")
+  rownames(Tm2) <- c("Intercept", "$B_n(t,lambda_1)$", "$B_n(t,lambda_2)$",
+                     "$B_n(t,lambda_3)$", "$B_n(t,lambda_4)$", "sigma_eps",
+                     "Edad", "Sexo", "Current Value", "Slope")
   #--------------#
   # Right column #
   #--------------#
-  value <- rbind(t(t(M3$postMeans$betas)),
-                 t(t(M3$postMeans$sigma)),
-                 t(t(M3$postMeans$gammas)),
+  value <- rbind(t(t(m3$postMeans$betas)),
+                 t(t(m3$postMeans$sigma)),
+                 t(t(m3$postMeans$gammas)),
                  NA,
-                 t(t(M3$postMeans$Dalphas)))
-  lower <- rbind(t(t(M3$CIs$betas[1,])),
-                 t(t(M3$CIs$sigma[1,])),
-                 t(t(M3$CIs$gammas[1,])),
+                 t(t(m3$postMeans$Dalphas)))
+  lower <- rbind(t(t(m3$CIs$betas[1, ])),
+                 t(t(m3$CIs$sigma[1, ])),
+                 t(t(m3$CIs$gammas[1, ])),
                  NA,
-                 t(t(M3$CIs$Dalphas[1,])))
-  upper <- rbind(t(t(M3$CIs$betas[2,])),
-                 t(t(M3$CIs$sigma[2,])),
-                 t(t(M3$CIs$gammas[2,])),
+                 t(t(m3$CIs$Dalphas[1, ])))
+  upper <- rbind(t(t(m3$CIs$betas[2, ])),
+                 t(t(m3$CIs$sigma[2, ])),
+                 t(t(m3$CIs$gammas[2, ])),
                  NA,
-                 t(t(M3$CIs$Dalphas[2,])))
-  intervals <- cbind(value,lower,upper)
-  colnames(intervals) <- c('value', 'lower', 'upper')
+                 t(t(m3$CIs$Dalphas[2, ])))
+  intervals <- cbind(value, lower, upper)
+  colnames(intervals) <- c("value", "lower", "upper")
   CI <- NULL
-  for(i in 1:10){
-    CI <- c(CI,paste0('(',round(lower[i],4),';',round(upper[i],4),')'))
+  for (i in 1:10) {
+    CI <- c(CI, paste0("(", round(lower[i], 4), ";", round(upper[i], 4), ")"))
   }
-  TM3 <- data.frame('Mean' = round(value,4), CI)
-  colnames(TM3) <- c('Mean', '95% CI')
-  rownames(TM3) <- c('Intercept', '$B_n(t,lambda_1)$', '$B_n(t,lambda_2)$',
-                     '$B_n(t,lambda_3)$', '$B_n(t,lambda_4)$', 'sigma_eps',
-                     'Edad', 'Sexo', 'Current Value', 'Slope')
-  #---------#
-  # Table C #
-  #---------#
-  summaryJM <- cbind(TM1,TM2,TM3)
-  DIC <- c(M1$DIC,M2$DIC,M3$DIC)
-  names(DIC)<-c('M1', 'M2', 'M3')
-  
-  return(list('summaryJM' = summaryJM, 'DIC' = DIC))
-  
+  Tm3 <- data.frame("Mean" = round(value, 4), CI)
+  colnames(Tm3) <- c("Mean", "95% CI")
+  rownames(Tm3) <- c("Intercept", "$B_n(t,lambda_1)$", "$B_n(t,lambda_2)$",
+                     "$B_n(t,lambda_3)$", "$B_n(t,lambda_4)$", "sigma_eps",
+                     "Edad", "Sexo", "Current Value", "Slope")
+  #---------------#
+  # summary table #
+  #---------------#
+  summaryJM <- cbind(Tm1, Tm2, Tm3)
+  DIC <- c(m1$DIC, m2$DIC, m3$DIC)
+  names(DIC) <- c("m1", "m2", "m3")
+  return(list("summaryJM" = summaryJM, "DIC" = DIC))
 }
