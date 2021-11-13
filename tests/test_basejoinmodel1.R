aa183e372588c96ad665f86aa3ec6aa187d5c3ab
ainsausti008 <ainsausti008@gmail.com>
2021-11-13 12:05:19 +0100

    refactor tests

--- tests/test_basejoinmodel1.R
+++ tests/test_basejoinmodel1.R
@@ -1,11 +1,13 @@
-source("utils/df_utils.R")
-DATAOUTPATH <- "data/out/"
-base_join_model1_0 <- readRDS(paste0(DATAOUTPATH, "baseJoinModel_afterMonthlyRearrangement.rds"))
-
-
-for (i in c(1:nrow(base_join_model1_0) )) {
-  v <- collapsedstring_tovector(as.character(base_join_model1_0[i, c('days')]))
-  if ((length(v) < 30) & (length(v) > as.numeric(base_join_model1_0[i, c('duration')]))) {
-    print(i)
+comprobar_coherencia_prescripciones_cortas <- function(base_join_model1_0){
+  for (i in c(1:nrow(base_join_model1_0) )) {
+    v <- collapsedstring_tovector(as.character(base_join_model1_0[i, c('days')]))
+    if ((length(v) < 30) & (length(v) > as.numeric(base_join_model1_0[i, c('duration')]))) {
+      print(i)
+    }
   }
 }
+
+test_that("testear coherencia en prescripciones cortas", {
+  expect_success(expect_output(comprobar_coherencia_prescripciones_cortas(base_join_model1_0), NA))
+}
+)
--- tests/testthat.R
+++ tests/testthat.R
@@ -1,21 +1,15 @@
 library(testthat)
-source("tests/test_jmdf.R")
-source("tests/test_basejoinmodel1.R")
+# source("tests/test_jmdf.R")
+# source("tests/test_basejoinmodel1.R")
 source("utils/df_utils.R")
 
-df_jm <- readr::read_csv("data/out/df_JM.csv")
-base_join_model1_0 <- readRDS("data/out/baseJoinModel_afterMonthlyRearrangement.rds")
+DATAOUTPATH <- "data/out"
+df_jm <- readr::read_csv(paste(DATAOUTPATH, 'df_JM.csv', sep = '/'))
+base_join_model1_0 <- readRDS(paste(DATAOUTPATH, 'baseJoinModel_afterMonthlyRearrangement.rds', sep = '/'))
 
 # ***************************************************************************
 
-# show_failure(expect_output(comprobar_valores(), NA))
-
-test_that("test de prueba",{
-  #expect_success(expect_output(hola(), "Hola"))
-  expect_success(expect_output(length_duration(), NA))
-  expect_success(expect_output(comprobar_valores_rango(), NA))
-  expect_success(expect_output(comprobar_valores_tiempo(), NA))
-  expect_success(expect_output(comprobar_valores_perc(), NA))
-})
+test_file("tests/test_jmdf.R")
+test_file("tests/test_basejoinmodel1.R")
 
 # ***************************************************************************
