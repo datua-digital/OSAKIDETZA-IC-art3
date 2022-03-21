3e618da0089b57bdae8f490648ddef93a4faa33e
ainsausti008 <ainsausti008@gmail.com>
2022-03-21 08:10:25 +0100

    Hacer funcionar calculo de AUC para modelo multivariante de tres variables longitudinales

--- src/modelling_mv_jm.R
+++ src/modelling_mv_jm.R
@@ -129,38 +129,78 @@ apply_MV2JM(
 )
 
 
+# Joint model multivariante, 3 variables longitudinales -------------------
 
-
-
-
-
-apply_MV3JM <- function(df_jm0, patients_conditions, VARIABLESCOX_IND, VARIABLESCOX,
-                        VARIABLESTODOS, OUTPATH, output = 'MV3JM') {
-  #PARA TRES VARIABLES LONGITUDINALES
+apply_MV3JM <- function(
+    df_jm, 
+    patients_conditions, 
+    covariables, 
+    model_name_prefix = 'JMMV3', 
+    save_model = FALSE
+) {
+  
+  # fixed variables: 
+  variables_ids_eventos <- c("id", "event", "time_to_event", "month")
+  variables_longitudinales <- c("cum_perc_adh_ara2oieca", "cum_perc_adh_bbloq", "cum_perc_adh_arm")  # es necesario para que no de un error en jointModelBayes
+  
+  # build and preprocess data
+  df_jm <- filter_patients(df_jm, patients_conditions)
+  # df_jm <- df_jm[df_jm$id %in% unique(df_jm$id)[1:300], ]
+  df_jm <- preprocess_dfjm(
+    df_jm, 
+    variables_jm = c(covariables, variables_longitudinales, variables_ids_eventos)
+  )
+  df_jm <- as.data.frame(df_jm)
+  cox_df <- generate_coxdf(
+    df_jm,
+    variables_cox = c(covariables, variables_ids_eventos)
+  )
+  
   # Lista de ecuaciones con su respectivas familiasa
   ecuaciones <- list(
-    as.formula(paste(paste("cum_perc_adh_ara2oieca" ,paste('ns(month, 4)', collapse = '+'),  sep = '~'), "(ns(month, 4) | id)", sep = '+')),
-    as.formula(paste(paste("cum_perc_adh_bbloq" ,paste('ns(month, 4)', collapse = '+'),  sep = '~'), "(ns(month, 4) | id)", sep = '+')),
-    as.formula(paste(paste("cum_perc_adh_arm" ,paste('ns(month, 4)', collapse = '+'),  sep = '~'), "(ns(month, 4) | id)", sep = '+'))
+    as.formula(
+      paste(
+        paste(
+          "cum_perc_adh_ara2oieca", paste('ns(month, 4)', collapse = '+'),  sep = '~'
+        ),
+        "(ns(month, 4) | id)", sep = '+'
+      )
+    ),
+    as.formula(
+      paste(
+        paste(
+          "cum_perc_adh_bbloq", paste('ns(month, 4)', collapse = '+'),  sep = '~'
+        ), "(ns(month, 4) | id)", sep = '+'
+      )
+    ),
+    as.formula(
+      paste(
+        paste("cum_perc_adh_arm", paste('ns(month, 4)', collapse = '+'),  sep = '~'
+              ), "(ns(month, 4) | id)", sep = '+'
+        )
+      )
   )
+  
   family <- list(gaussian, gaussian,gaussian)
   
   # M1
   set.seed(1000)
-  MixedModelFit1 <- mvglmer(ecuaciones,
-                            data = df_jm,
-                            families = family)
+  MixedModelFit1 <- mvglmer(
+    ecuaciones,
+    data = df_jm,
+    families = family
+  )
   
-  surv_object <- Surv(time = cox_df$time_to_event,
-                      event = as.numeric(cox_df$event))
+  survFit <- coxph(
+    Surv(time_to_event, event) ~ sexo + edad_ing1 + charlson + fe.reducida.severa, 
+    data = cox_df, 
+    model = TRUE
+  )
   
-  coxFit.df_jm <- coxph(as.formula(paste("surv_object", paste(VARIABLESCOX, collapse = "+"), sep = "~")), 
-                        data = cox_df,
-                        x = TRUE,
-                        model = TRUE)
-
-  M1 <- mvJointModelBayes(MixedModelFit1, coxFit.df_jm, timeVar = "month")
+  
+  M1 <- mvJointModelBayes(MixedModelFit1, survFit, timeVar = "month")
   summary(M1)
+  
   saveRDS(M1, paste0(OUTPATH, output, "_M1", ".rds"))
   
   
@@ -193,45 +233,17 @@ apply_MV3JM <- function(df_jm0, patients_conditions, VARIABLESCOX_IND, VARIABLES
   saveRDS(M3, paste0(OUTPATH, output, "_M3", ".rds"))
 }
 
-# JM para adherencia guia (con arm) ---------------------------------------------------------------------
-# LONGVAR <- c("cum_perc_adh_guia_arm", "cum_perc_adh_bbloq")
 
-
-# TODO Ejecutar joint model
-# Subset: Muestra sin tener en cuenta pacientes que fallecen los primeros 30 días
-df_jm <- readr::read_csv("data/out/df_JM.csv")
-# y filtrando pacientes de novo en fecha ingreso
-patients_conditions <- list(
-  denovo_ic_paciente = TRUE,
-  denovo_tt_paciente_fing = TRUE,
-  denovo_tt_paciente_falta = NULL,
-  early_death_patient_30 = FALSE,
-  patient_with_prescription = NULL
+apply_MV3JM(
+  df_jm = readRDS(paste0(DATAOUTPATH, "df_JM.rds")), 
+  patients_conditions = list(
+    denovo_ic_paciente = NULL,
+    denovo_tt_paciente_fing = NULL,
+    denovo_tt_paciente_falta = NULL,
+    early_death_patient_30 = NULL,
+    patient_with_prescription = NULL
+  ), 
+  covariables = c("sexo", "edad_ing1", "charlson", "fe.reducida.severa"),
+  model_name_prefix = 'JMMV2', 
+  save_model = TRUE
 )
-apply_JM(df_jm0 = df_jm, patients_conditions, VARIABLESCOX_IND, VARIABLESCOX, VARIABLESTODOS, OUTPATH, output = 'MV2JM3')
-
-
-df_jm <- readr::read_csv("data/out/df_JM.csv")
-# Subset: Muestra teniendo en cuenta todos los id-s
-patients_conditions <- list(
-  denovo_ic_paciente = NULL,
-  denovo_tt_paciente_fing = NULL,
-  denovo_tt_paciente_falta = NULL,
-  early_death_patient_30 = NULL,
-  patient_with_prescription = NULL
-)
-
-apply_MV3JM(df_jm0 = df_jm, patients_conditions, VARIABLESCOX_IND, VARIABLESCOX, VARIABLESTODOS, OUTPATH, output = 'MV3JM')
-
-
-# Subset: Muestra filtrando pacientes con prescripciones,
-df_jm <- readr::read_csv("data/out/df_JM.csv")
-# sin tener en cuenta pacientes que fallecen los primeros 30 días, y filtrando pacientes de novo en fecha ingreso
-patients_conditions <- list(
-  denovo_ic_paciente = TRUE,
-  denovo_tt_paciente_fing = TRUE,
-  denovo_tt_paciente_falta = NULL,
-  early_death_patient_30 = FALSE,
-  patient_with_prescription = TRUE
-)
-apply_MV3JM(df_jm0 = df_jm, patients_conditions, VARIABLESCOX_IND, VARIABLESCOX, VARIABLESTODOS, OUTPATH, output = 'MV3JM3')
